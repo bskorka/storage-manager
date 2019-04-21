22a733c1fab8be86343e87f0b6e02678159f8083
Bartek <bartosz.skorka93@gmail.com>
2019-04-21 23:09:00 +0200

    New favicon and made full path to move in application

--- src/main/java/pl/bskorka/storagemanager/controller/WebAppController.java
+++ src/main/java/pl/bskorka/storagemanager/controller/WebAppController.java
@@ -4,17 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.Date;
-
 @Controller
 public class WebAppController {
 
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("datetime", new Date());
-        model.addAttribute("username", "Bartek");
-        model.addAttribute("mode", "super app mode");
-
         return "index";
     }
 
